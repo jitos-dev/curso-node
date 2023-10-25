db19e46ddb96e03f44a67e93de13771ae9ad10f0
jitos86 <jitos86@gmail.com>
2023-10-25 12:50:54 +0200

    feat: apply new indent spaces

--- app-clima/helpers/inquirer.js
+++ app-clima/helpers/inquirer.js
@@ -3,132 +3,141 @@ const inquirer = require("inquirer");
 require("colors");
 
 const questions = [
-  {
-    type: "list",
-    name: "opt",
-    message: "¿Que desea hacer?",
-    choices: [
-      { value: 1, name: `${"1.".green} Buscar ciudad` },
-      { value: 2, name: `${"2.".green} Historial` },
-      { value: 0, name: `${"0.".green} Salir\n` },
-    ],
-  },
+	{
+		type: "list",
+		name: "opt",
+		message: "¿Que desea hacer?",
+		choices: [
+			{ value: 1, name: `${"1.".green} Buscar ciudad` },
+			{ value: 2, name: `${"2.".green} Historial` },
+			{ value: 0, name: `${"0.".green} Salir\n` },
+		],
+	},
 ];
 
 const inquirerMenu = async () => {
-  //console.clear();
-  console.log("==============================".green);
-  console.log("    Seleccione una opción".white);
-  console.log("==============================".green);
+	//console.clear();
+	console.log("==============================".green);
+	console.log("    Seleccione una opción".white);
+	console.log("==============================".green);
 
-  const { opt } = await inquirer.prompt(questions);
+	const { opt } = await inquirer.prompt(questions);
 
-  return opt;
+	return opt;
 };
 
 const chooseOption = async (message) => {
-  const inputQuestion = [
-    {
-      type: "confirm",
-      name: "option",
-      message,
-    },
-  ];
+	const inputQuestion = [
+		{
+			type: "confirm",
+			name: "option",
+			message,
+		},
+	];
 
-  const { option } = await inquirer.prompt(inputQuestion);
+	const { option } = await inquirer.prompt(inputQuestion);
 
-  return option;
+	return option;
 };
 
 const readInput = async (message) => {
-  const input = [
-    {
-      type: "input",
-      name: "option",
-      message,
-      // Si queremos validar lo que ha introducido el usuario lo hacemos así
-      // validate(value) {
-      //   if (value.length === 0) {
-      //     return "Por favor ingrese el valor solicitado";
-      //   }
+	const input = [
+		{
+			type: "input",
+			name: "option",
+			message,
+			// Si queremos validar lo que ha introducido el usuario lo hacemos así
+			// validate(value) {
+			//   if (value.length === 0) {
+			//     return "Por favor ingrese el valor solicitado";
+			//   }
 
-      //   return true;
-      // },
-    },
-  ];
+			//   return true;
+			// },
+		},
+	];
 
-  const { option } = await inquirer.prompt(input);
+	const { option } = await inquirer.prompt(input);
 
-  return option;
+	return option;
 };
 
 const deleteTaskMenu = async (tasks = []) => {
-  if (tasks.length === 0) {
-    console.log('No hay tareas para borrar'.red);
-    console.log();
-    return;
-  }
+	if (tasks.length === 0) {
+		console.log("No hay tareas para borrar".red);
+		console.log();
+		return;
+	}
 
-  // Creamos un array con todas las tareas para borrar y la última opción salir sin borrar nada
-  let index = 0
-  let choices = listTasks(tasks, index);
+	// Creamos un array con todas las tareas para borrar y la última opción salir sin borrar nada
+	let index = 0;
+	let choices = listTasks(tasks, index);
 
-  // Opción de salir sin borrar
-  choices.push({
-    value: 0,
-    name: `${(choices.length + 1).toString().green + '.'.green} Salir sin borrar tarea`
-  })
+	// Opción de salir sin borrar
+	choices.push({
+		value: 0,
+		name: `${
+			(choices.length + 1).toString().green + ".".green
+		} Salir sin borrar tarea`,
+	});
 
-  const questions = [
-    {
-      type: 'list',
-      name: 'option',
-      message: 'Seleccione la tarea que desea eliminar',
-      choices
-    }
-  ]
+	const questions = [
+		{
+			type: "list",
+			name: "option",
+			message: "Seleccione la tarea que desea eliminar",
+			choices,
+		},
+	];
 
-  const { option } = await inquirer.prompt(questions)
-  
-  return option;
-}
+	const { option } = await inquirer.prompt(questions);
+
+	return option;
+};
 
 const completeTasksList = async (tasks = []) => {
-  if (tasks.length === 0) {
-    console.log('No hay tareas para mostrar'.red);
-    return;
-  }
+	if (tasks.length === 0) {
+		console.log("No hay tareas para mostrar".red);
+		return;
+	}
 
-  // Creamos un array con todas las tareas
-  let index = 0
-  let choices = listTasks(tasks, index);
+	// Creamos un array con todas las tareas
+	let index = 0;
+	let choices = listTasks(tasks, index);
 
-  const questions = [
-    {
-      type: 'checkbox',
-      name: 'options',
-      message: 'Marca la o las tareas que quieres completar, \ndesmarca la o las tareas que quieres pasar a pendientes',
-      choices
-    }
-  ]
+	const questions = [
+		{
+			type: "checkbox",
+			name: "options",
+			message:
+				"Marca la o las tareas que quieres completar, \ndesmarca la o las tareas que quieres pasar a pendientes",
+			choices,
+		},
+	];
 
-  const { options } = await inquirer.prompt(questions)
+	const { options } = await inquirer.prompt(questions);
 
-  return options
-}
+	return options;
+};
 
 const listTasks = (tasks = [], index) => {
-  let choices = tasks.map(task => {
-    index ++;
+	let choices = tasks.map((task) => {
+		index++;
 
-    return {
-      value: task.id,
-      name: `${index.toString().green + '.'.green} ${task.description}`,
-      checked: task.completedIn ? true : false
-    }
-  })
+		return {
+			value: task.id,
+			name: `${index.toString().green + ".".green} ${task.description}`,
+			checked: task.completedIn ? true : false,
+		};
+	});
 
-  return choices;
-}
+	return choices;
+};
 
-module.exports = {inquirerMenu, chooseOption , readInput, deleteTaskMenu, completeTasksList}
+module.exports = {
+	inquirerMenu,
+	chooseOption,
+	readInput,
+	deleteTaskMenu,
+	completeTasksList,
+};
--- app-clima/index.js
+++ app-clima/index.js
@@ -1,24 +1,20 @@
-const { inquirerMenu, chooseOption } = require("./helpers/inquirer")
-require('colors')
+const { inquirerMenu, chooseOption } = require("./helpers/inquirer");
+require("colors");
 
 const main = async () => {
+	let opt = 0;
+	do {
+		opt = await inquirerMenu();
 
-    let opt = 0;
-    do {
-        opt = await inquirerMenu()
+		if (opt === 0) {
+			const exit = await chooseOption("¿Seguro que quieres salir?");
 
+			if (!exit) {
+				opt = 1;
+			}
+		}
+	} while (opt != 0);
+};
 
-        if (opt === 0) {
-            const exit = await chooseOption('¿Seguro que quieres salir?')
-            
-            if (!exit) {
-                opt = 1;
-            }
-        }
-
-    } while(opt != 0);
-
-}
-
-main()
-console.log('Bienvenido a la app'.green);
+main();
+console.log("Bienvenido a la app".green);
