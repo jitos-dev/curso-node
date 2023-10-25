51a065a85f71f1a68af4dead1c955df44cbbaf63
jitos86 <jitos86@gmail.com>
2023-10-25 12:23:45 +0200

    feat: show menu when run app and exit option

--- app-clima/index.js
+++ app-clima/index.js
@@ -1,3 +1,24 @@
+const { inquirerMenu, chooseOption } = require("./helpers/inquirer")
 require('colors')
 
+const main = async () => {
+
+    let opt = 0;
+    do {
+        opt = await inquirerMenu()
+
+
+        if (opt === 0) {
+            const exit = await chooseOption('¿Seguro que quieres salir?')
+            
+            if (!exit) {
+                opt = 1;
+            }
+        }
+
+    } while(opt != 0);
+
+}
+
+main()
 console.log('Bienvenido a la app'.green);
