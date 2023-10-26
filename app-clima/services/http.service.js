2ee8b7358c3daa505e29b23064f45227239a5371
jitos86 <jitos86@gmail.com>
2023-10-26 12:20:52 +0200

    feat(http.service): service for the http requests with the axios library

---
+++ app-clima/services/http.service.js
@@ -0,0 +1,24 @@
+const axios = require('axios')
+const env = require('../helpers/enviroments')
+
+class HttpService {
+
+    get(url, config) {
+        return axios.get(url, config)
+    }
+
+    post(url, data, config) {
+        return axios.post(url, data, config)
+    }
+
+
+    put(url, data, config) {
+        return axios.patch(url, data, config)
+    }
+
+    delete(url, config) {
+        return axios.delete(url, config)
+    }
+}
+
+module.exports = HttpService;
