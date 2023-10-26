c9659d521d072217d6592d2f2176eddc2d79ab3a
jitos86 <jitos86@gmail.com>
2023-10-26 16:25:21 +0200

    feat(request.service.js): check the errors with try catch block

--- app-clima/services/request.service.js
+++ app-clima/services/request.service.js
@@ -5,25 +5,37 @@ const httpService = require('./http.service')
 class RequestService {
 
     async getLocation(city) {
-        const baseUrl = enviroments.locationIqUrl;
-        const limit = enviroments.locationIqLimit;
-        const language = enviroments.locationIqLanguage;
-        const key = enviroments.locationIqToken;
 
-        let {data, status } = await httpService.get(baseUrl, {
-            headers: {'accept': 'application/json'},
-            params: {
-                q: city,
-                format: 'json',
-                limit,
-                'accept-lange': language,
-                key
+        try {
+            const baseUrl = enviroments.locationIqUrl;
+            const limit = enviroments.locationIqLimit;
+            const language = enviroments.locationIqLanguage;
+            const key = enviroments.locationIqToken;
+    
+            let {data, status } = await httpService.get(baseUrl, {
+                headers: {'accept': 'application/json'},
+                params: {
+                    q: city,
+                    format: 'json',
+                    limit,
+                    'accept-lange': language,
+                    key
+                }
+            })
+    
+            data = formatLocationData(data)
+    
+            return {data, status}
+
+        } catch (error) {
+            const { status } = error.response;
+            
+            return {
+                data: [],
+                status
             }
-        })
+        }
 
-        data = formatLocationData(data)
-
-        return {data, status}
     }
 
     
