d11cefa59e5b42d15de5600a444097e1135a3f8a
jitos86 <jitos86@gmail.com>
2023-10-27 12:39:52 +0200

    feat: add openWeather enviroments

--- app-clima/helpers/enviroments.js
+++ app-clima/helpers/enviroments.js
@@ -10,6 +10,9 @@ const env = {
 	locationIqUrl: process.env.LOCATION_IQ_URL,
 	locationIqLimit: process.env.LOCATION_IQ_LIMIT,
 	locationIqLanguage: process.env.LOCATION_IQ_LANGUAGE,
+	openWeatherKey: process.env.OPENWEATHER_KEY,
+	openWeatherUrl: process.env.OPENWEATHER_URL,
+	openWeatherLanguage: process.env.OPENWEATHER_LANGUAGE,
 };
 
 module.exports = env;
